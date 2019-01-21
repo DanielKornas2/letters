a3bb0ce2dc9b283b9807067c3ae31c99411d8c33
Daniel Kornas <harvi_pl@wp.pl>
2019-01-21 12:20:43 +0100

    Split letters, delete items, set new state and re-render component

--- src/App.js
+++ src/App.js
@@ -18,13 +18,25 @@ class App extends Component {
     })
   }
 
+  removeLetter = (index) => {
+    const singleLetters = this.state.text.split(""); // split state text to single letters
+    singleLetters.splice(index,1); // delete clicked item - i found this by clicked index
+    const newText = singleLetters.join(""); // create new text - by joining single letters without clicked letter 
+    this.setState({
+      text: newText
+    })
+  }
+
   render() {
     const singleLetters = this.state.text.split("");
     return (
       <React.Fragment>
+        <h1>Letters app</h1>
+        <p>How does it work?</p>
+        <p>Specify your text. You'll see single letters and if you click on single letter you simply delete it.</p>
         <input type="text" value={this.state.text} onChange={this.handleInputChange} />
         <ValidationComponent textLength = {this.state.text.length} />
-        {singleLetters.map((letter, index) => <CharComponent letter={letter} key={index}/>)}
+        {singleLetters.map((letter, index) => <CharComponent letter={letter} key={index} removeLetter ={() => this.removeLetter(index)} />)}
       </React.Fragment>
     );
   }
--- src/CharComponent/CharComponent.js
+++ src/CharComponent/CharComponent.js
@@ -10,7 +10,7 @@ const charComponent = (props) => {
     }
 
     return (
-        <div style={style}>{props.letter}</div>
+        <div style={style} onClick={props.removeLetter}>{props.letter}</div>
     )
 }
 
