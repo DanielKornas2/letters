d2e5a032c1e515b6f72f24dd924f48d8b45ed262
Daniel Kornas <harvi_pl@wp.pl>
2019-01-21 11:19:03 +0100

    New input, input handler and correct state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      text : ""
+    }
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      text : e.target.value
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <input type="text" value={this.state.text} onChange={this.handleInputChange} />
+      </React.Fragment>
     );
   }
 }
